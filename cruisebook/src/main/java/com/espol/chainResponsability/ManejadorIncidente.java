94e4a910a374ba2b304e74a645234a1679caa660
Anahi Quezada <70460216+Anie2490@users.noreply.github.com>
2025-12-19 13:31:49 -0500

    Update ManejadorIncidente.java

--- cruisebook/src/main/java/com/espol/chainResponsability/ManejadorIncidente.java
+++ cruisebook/src/main/java/com/espol/chainResponsability/ManejadorIncidente.java
@@ -2,13 +2,27 @@ package com.espol.chainResponsability;
 
 import com.espol.entidades.Incidente;
 
-public abstract class ManejadorIncidente {
-
+public abstract class ManejadorIncidente implements BaseHandler {
     protected ManejadorIncidente siguiente;
-
-    public void setSiguiente(ManejadorIncidente siguiente) {
-        this.siguiente = siguiente;
+    
+    @Override
+    public void setSiguiente(BaseHandler handler) {
+        this.siguiente = (ManejadorIncidente) handler;
     }
-
+    
+    public void setSiguiente(ManejadorIncidente manejador) {
+        this.siguiente = manejador;
+    }
+    
+    @Override
     public abstract void manejarIncidente(Incidente incidente);
+    
+    @Override
+    public void procesar(Incidente incidente) {
+        if (puedeManejar(incidente)) {
+            manejarIncidente(incidente);
+        } else if (siguiente != null) {
+            siguiente.procesar(incidente);
+        }
+    }
 }
